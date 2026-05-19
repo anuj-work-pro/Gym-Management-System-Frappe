fd06ed1586b7adb2db3bc427abd949f8a780213b
anuj-work-pro <anuj.tomar@promantia.com>
2026-05-19 16:05:54 +0530

    update dashboard, adding tests

--- gym_management_system/public/js/gym_membership.js
+++ gym_management_system/public/js/gym_membership.js
@@ -1,6 +1,5 @@
 frappe.ui.form.on("Gym Membership", {
 	refresh(frm) {
-		frappe.msgprint("Thankyou for choosing us, kindly fill the form");
+		frappe.msgprint(frappe._("Thankyou for choosing us, kindly fill the form"));
 	},
 });
-
